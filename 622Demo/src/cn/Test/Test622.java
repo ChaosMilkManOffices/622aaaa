8447f329dda2b51caf27eb3b5879390a2102aa20
JADA <1617771508@qq.com>
2020-06-22 16:50:30 +0800

    JASON第一次提交

--- 622Demo/src/cn/Test/Test622.java
+++ 622Demo/src/cn/Test/Test622.java
@@ -4,7 +4,8 @@ public class Test622 {
 public static void main(String[] args) {
 	System.out.println("???????");
 	System.out.println("jjjjjjjjjjjjjj");
-	System.out.println("ßÇßÇßÇßÇßÇßÇÀ©À©À©À©");
-	System.out.println("ºÙºÙºÙ  ÎÒËµgit  ÄãËµÒª git yao");
+	System.out.println("ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½");
+	System.out.println("ï¿½ÙºÙºï¿½  ï¿½ï¿½Ëµgit  ï¿½ï¿½ËµÒª git yao");
+	System.out.println("ä¸‰å·è®¿å®¢JASON>>>>>>>>é€€å‡º");
 }
 }
