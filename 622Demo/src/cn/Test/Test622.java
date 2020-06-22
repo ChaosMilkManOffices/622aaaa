ab7c4519c867d7fd9979adc34bac123c95cbe56f
kevin <785145898@qq.com>
2020-06-22 17:18:51 +0800

    ........

--- 622Demo/src/cn/Test/Test622.java
+++ 622Demo/src/cn/Test/Test622.java
@@ -8,5 +8,6 @@ public static void main(String[] args) {
 	System.out.println("�ٺٺ�  ��˵git  ��˵Ҫ git yao");
 	System.out.println("三号访客JASON>>>>>>>>退出");
 	System.out.println("测试提交>>>>>>>结束");
+	System.out.println("111111");
 }
 }
