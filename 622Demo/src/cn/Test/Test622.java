b83c2146071265e18340206cfc0ad16043bab690
kevin <785145898@qq.com>
2020-06-22 16:35:34 +0800

    小盘第一次提交

--- 622Demo/src/cn/Test/Test622.java
+++ 622Demo/src/cn/Test/Test622.java
@@ -4,5 +4,6 @@ public class Test622 {
 public static void main(String[] args) {
 	System.out.println("???????");
 	System.out.println("jjjjjjjjjjjjjj");
+	System.out.println("二号访客小盘进入>>>>>>>>>>>退出");
 }
 }
