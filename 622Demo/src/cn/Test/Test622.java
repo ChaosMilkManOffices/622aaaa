2c0d43a63147ddf075c6fb33a6ac6fe5f9bced74
kevin <785145898@qq.com>
2020-06-22 17:11:48 +0800

    小盘第二次提交

--- 622Demo/src/cn/Test/Test622.java
+++ 622Demo/src/cn/Test/Test622.java
@@ -7,5 +7,6 @@ public static void main(String[] args) {
 	System.out.println("��������������������");
 	System.out.println("�ٺٺ�  ��˵git  ��˵Ҫ git yao");
 	System.out.println("三号访客JASON>>>>>>>>退出");
+	System.out.println("测试提交>>>>>>>结束");
 }
 }
